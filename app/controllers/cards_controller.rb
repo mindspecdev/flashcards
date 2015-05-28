6c8d35800737ca6fa2b702b98eabd8b0081c1efb
Denis Sergeev <dvsergeev@gmail.com>
2015-05-27 22:36:14 -0400

    Updated card validation and controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,70 +1,70 @@
 class CardsController < ApplicationController
 
   def index
-    load_resources
+    load_cards
   end
 
   def show
-    load_resource
+    load_card
   end
 
   def new
-    build_resource
+    build_card
   end
 
   def create
-    build_resource
-    save_resource or render 'new'
+    build_card
+    save_card or render 'new'
   end
 
   def edit
-    load_resource
-    build_resource
+    load_card
+    build_card
   end
 
   def update
-    load_resource
-    build_resource
-    save_resource or render 'edit' 
+    load_card
+    build_card
+    save_card or render 'edit' 
   end
 
   def destroy
-    load_resource
-    @resource.destroy
+    load_card
+    @card.destroy
     redirect_to cards_path
   end
 
   private
 
-  def load_resources
-    @resources ||= resource_scope.to_a
+  def load_cards
+    @cards ||= card_scope.to_a
     respond_to do |format|
       format.html
       format.json
     end    
   end
 
-  def load_resource
-    @resource ||= resource_scope.find(params[:id])
+  def load_card
+    @card ||= card_scope.find(params[:id])
   end
 
-  def build_resource
-    @resource ||= resource_scope.build
-    @resource.attributes = resource_params
+  def build_card
+    @card ||= card_scope.build
+    @card.attributes = card_params
   end
 
-  def save_resource
-    if @resource.save
+  def save_card
+    if @card.save
       redirect_to cards_path
     end
   end
 
-  def resource_params
-    resource_params = params[:card]
-    resource_params ? resource_params.permit(:original_text, :translated_text, :review_date) : {}
+  def card_params
+    card_params = params[:card]
+    card_params ? card_params.permit(:original_text, :translated_text, :review_date) : {}
   end
 
-  def resource_scope
+  def card_scope
     Card.all
   end
 end
