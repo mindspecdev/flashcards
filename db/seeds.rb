ead5acebe83ade7898a8ff0c4f383c7fb7be98f1
Denis Sergeev <dvsergeev@gmail.com>
2015-06-11 22:47:08 -0400

    Updated fourth task for {}

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@ Card.delete_all
 url = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words"
 
 doc = Nokogiri::HTML(open(url))
-doc.css('div.jsn-article-content table tbody tr').each  { |word|
+doc.css('div.jsn-article-content table tbody tr').each do |word|
   original_text = word.css('td.bigLetter')
   translated_text = word.css('td')[2]  
   if original_text
     Card.create(original_text: original_text.text, translated_text: translated_text.text, review_date: '2015-01-01')
   end
-}
+end
